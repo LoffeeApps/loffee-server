04f52101c92c2552a3dcb976351f0297f9a20dea
pramesthias <oka.pramesthi@gmail.com>
2023-11-21 22:50:50 +0700

    Feat: authentication

---
+++ middlewares/authentication.js
@@ -0,0 +1,29 @@
+const { verifyToken } = require("../helpers/jwt");
+const { User } = require("../models");
+
+
+async function authentication(req, res, next){
+    try {
+        let access_token = req.headers.authorization
+
+        if(!access_token) throw {name: "Unauthenticated"} 
+
+        const { id } = verifyToken(access_token.slice(7))
+
+        let user = await User.findByPk(id)
+
+        if(!user) throw {name: "Unauthenticated"} 
+
+        req.user = {
+            id: user.id,
+            gender: user.gender
+        }
+
+        next();
+
+    } catch (error) {
+        next(error);
+    }
+}
+
+module.exports = authentication
