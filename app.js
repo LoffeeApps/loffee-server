ffce6729a8c809da4e31f960f0f3b98109ca776c
luluswahyu99 <luluswahyu99.lw@gmail.com>
2023-11-21 23:07:26 +0700

    socket: init

--- app.js
+++ app.js
@@ -7,6 +7,30 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
+const server = createServer(app)
+const io = new Server(server, {
+    cors: {
+        origin: "*"
+    }
+ });
+
+ io.on("connection", (socket) => {
+    console.log("new connection", socket.id)
+
+    socket.emit("test1", socket)
+
+    io.emit("test1", {message: `aku join dong, id ku ${socket.id}`})
+
+    socket.on("disconnect", () => {
+        console.log(`Halo ID ${socket.id} is disconnected`)
+    })
+
+    
+ })
+
 app.use(cors())
 
 app.use(express.urlencoded({ extended: true }));
@@ -17,4 +41,4 @@ app.use(require("./routes"));
 
 
 
-module.exports = app;
+module.exports = server;
